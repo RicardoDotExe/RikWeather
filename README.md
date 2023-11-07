8c834f549ed69ffcd83067f9155659589f25ac86
RicardoDotExe <ricardo.anton@gmail.com>
2023-11-07 18:45:44 +0100

    first-app-steps

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# React + Vite
+# APP CURRENT WEATHER
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Develop a WebApp that shows the actual weather in the actual location. The actual location must be obtain with the IP address.
 
-Currently, two official plugins are available:
+Weather API: https://open-meteo.com/en/docs
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+_The API is free with a cap of 10'000 requests._
+
+## Design
+
+The web design should be as close as possible to the following Figma design:
+
+- [Figma design](https://www.figma.com/file/4CjDBeUOxwDnUjF01CuCxP/Weather-Forecast-(Community)?type=design&node-id=0%3A1&mode=design&t=ije4fGuslXt7KA9o-1)
+
+Note: The Sun icon is orientative. The should be changing to match the actual weather (rain, sunny, cloudy).
+
+## Requirements
+
+Feel free to use the frameworks/libraries that you like. The only requirements are the following:
+
+- App develop with `react`.
+- Create the project with [Vite](https://vitejs.dev/) or any framework or tool that uses Vite for the bundle and compilation.
+- The project must use `git` for the control version. Upload to Github or Gitlab.
+- Remember, you can use whatever the framework/libraries you like. But all the above requirements have to be compatible with the libraries or frameworks you use.
+
+## Nice to have
+
+- Use Tailwind
+- Use of Docker for the develop environment.
+- Hourly weather info for the current location (only the next 12 hours).
+- Implements cache for the requests. It should not make the request if the data will be the same.
+- Testing for the React components and app.
+- Readme with the instructions for the project execution.+-
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [weatherData, setWeatherData] = useState(null);
+  
+
+  useEffect(() => {
+    // Realiza la solicitud a la API cuando el componente se monta
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('La solicitud a la API falló');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setWeatherData(data);
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error al obtener datos del clima:', error);
+      });
+  }, []);
+
+  
   return (
-    <>
+    <div className="card rounded-xl">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Madrid</h1>
+        <p className="left-0">Spain</p>
+        <div className="flex flex-row items-center">
+            <img src="media/Sol.png" alt="Hace Sol"></img>
+            <p className="ml-4 text-5xl" >  {weatherData.current.temperature_2m} {weatherData.current_units.temperature_2m}</p>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="name">Humedad</div>
+          <div className="datos"> {weatherData.current.relative_humidity_2m} </div>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="name">Viento</div>
+          <div className="datos"> {weatherData.current.wind_speed_10m} km</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 

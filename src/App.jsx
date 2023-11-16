25408497d4137a84b7b3861fb9ebdc6044e1a989
RicardoDotExe <ricardo.anton@gmail.com>
2023-11-16 16:58:08 +0100

    Añadido setear coordenadas desde IP

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,57 @@ import { useState, useEffect } from "react";
 
 function App() {
 
+  const [mensajeEspera, setMensajeEspera] = useState(null)
   const [weatherData, setWeatherData] = useState(null);
-  
+  const [IPData, setIPData] = useState(null);
 
   useEffect(() => {
-    // Realiza la solicitud a la API cuando el componente se monta
-    fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('La solicitud a la API falló');
-        }
-        return response.json();
-      })
-      .then(data => {
-        setWeatherData(data);
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error al obtener datos del clima:', error);
-      });
-  }, []);
+    setMensajeEspera("Cargando datos...")
 
+      fetch("https://ipinfo.io/79.116.62.157?token=567ac2ea1e6487")
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('La solicitud a la API IP falló');
+          }
+          return response.json();
+        })
+        .then(localData => {
+          setIPData(localData);
+          console.log(localData)
+           // Realiza la solicitud a la API del tiempo
+           const [lat, lon] = localData.loc.split(',');
+
+           console.log(lat);
+           console.log(lon);
+          fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m`)
+            .then(response => {
+            if (!response.ok) {
+              throw new Error('La solicitud a la API falló');
+            }
+            return response.json(); 
+            })
+          .then(wData => {
+            setWeatherData(wData);
+            console.log(wData);
+          })
+          //Finaliza solicitud a API del tiempo
+          .catch(error => {
+            console.error('Error al obtener datos del clima:', error);
+          });
+          })
+        .catch(error => {
+          console.error('Error al obtener datos del clima:', error);
+        });
+
+        console.log("uwu")
+     
+  }, []);
   
   return (
     <div className="card rounded-xl">
-      <div>
-        <h1>Madrid</h1>
+      {weatherData ? (
+        <div>
+        <h1>{IPData.city}</h1>
         <p className="left-0">Spain</p>
         <div className="flex flex-row items-center">
             <img src="media/Sol.png" alt="Hace Sol"></img>
@@ -42,7 +67,9 @@ function App() {
           <div className="name">Viento</div>
           <div className="datos"> {weatherData.current.wind_speed_10m} km</div>
         </div>
-      </div>
+        </div>
+      ) : (<p>{mensajeEspera}</p>
+      )}
     </div>
   )
 }

7adb87174f774e875431783ab9963a1c982351b7
RicardoDotExe <ricardo.anton@gmail.com>
2023-11-22 01:19:08 +0100

    Añadidos iconos y descripciones del tiempo

--- src/API/WeatherAPI.jsx
+++ src/API/WeatherAPI.jsx
@@ -1,7 +1,48 @@
 export default class WheatherAPI {
 
   static async getWeatherInfo(lat, lon) {
-    console.log('si')
     return await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m`)
+  }
+
+  static getImgYDescTiempo(weather_code, temperature, units) {
+    var preUrlMedia = 'src/assets/';
+    var imgCode;
+    var textWeather;
+
+    if (weather_code == 0) {
+      imgCode = '0.png'
+      textWeather = 'Soleado'
+    } else if (weather_code > 0 && weather_code < 3) {
+      imgCode = '1.png'
+      textWeather = 'ligeramente nublado'
+    } else if (weather_code == 3) {
+      imgCode = '3.png'
+      textWeather = 'Nublado'
+    } else if (weather_code > 3 && weather_code < 51) {
+      imgCode = '45.png'
+      textWeather = 'Niebla'
+    } else if (weather_code > 50 && weather_code < 61) {
+      imgCode = '51.png'
+      textWeather = 'Llovizna'
+    } else if ( weather_code > 60 && weather_code < 71) {
+      imgCode = '61.png'
+      textWeather = 'Lluvia'
+    } else if (weather_code > 70 && weather_code < 95){
+      imgCode = '71.png'
+      textWeather = 'Nevada'
+    } else if (weather_code > 94) {
+      imgCode = '95.png'
+      textWeather = 'Tormenta'
+    }
+
+    return (
+      <div className="flex flex-row items-center">
+        <img src={preUrlMedia+imgCode} alt={textWeather}></img>
+        <div>
+          <p className='ml-5 text-2xl'>{textWeather}</p>
+          <p className="ml-4 text-5xl" > {temperature} {units}</p>
+        </div>
+      </div>
+      )
   }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,14 @@ function App() {
     if (!IPData) {
       return false;
     }
-    console.log('amos loco')
+
     const [lat, lon] = IPData.loc.split(",");
     console.log([lat, lon])
     const callWeatherAPI = await WeatherAPI.getWeatherInfo(lat, lon)
     const dataWeather = await callWeatherAPI.json()
     setWeatherData(dataWeather)
+    console.log(dataWeather)
+    WeatherAPI.getImgYDescTiempo(dataWeather.current.weather_code, weatherData.current.temperature_2m, weatherData.current_units.temperature_2m)
   }, [IPData])
 
   useEffect(() => {
@@ -46,10 +48,7 @@ function App() {
         <div>
         <h1>{IPData.city}</h1>
         <p className="left-0">{IPData.country}</p>
-        <div className="flex flex-row items-center">
-            <img src="src/assets/Sol.png" alt="Hace Sol"></img>
-            <p className="ml-4 text-5xl" >  {weatherData.current.temperature_2m} {weatherData.current_units.temperature_2m}</p>
-        </div>
+        {WeatherAPI.getImgYDescTiempo(weatherData.current.weather_code, weatherData.current.temperature_2m, weatherData.current_units.temperature_2m)}
         <div className="grid grid-cols-2">
           <div className="name">Humedad</div>
           <div className="datos"> {weatherData.current.relative_humidity_2m} </div>

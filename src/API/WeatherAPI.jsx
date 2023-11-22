06fcf515eac54fad2ace0944cc03294d317eb69d
Victor Esteban <victor.esteban@uponor.com>
2023-11-22 16:38:24 +0100

    Add comments

--- src/API/WeatherAPI.jsx
+++ src/API/WeatherAPI.jsx
@@ -2,6 +2,8 @@ export default class WheatherAPI {
 
   static async getWeatherInfo(lat, lon) {
     console.log('si')
+    // The API string needs to be parametrized. It should be like IpAPI.
+    // Also it would be nice to have the possibility to have the api options in a variable.
     return await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m`)
   }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import { useState, useEffect, useCallback } from "react";
 
 function App() {
 
+  // Variable name in spanish 👿
   const [mensajeEspera, setMensajeEspera] = useState(null)
   const [IPData, setIPData] = useState(null)
   const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
+    // The string should be in a constant.
     setMensajeEspera('Cargando datos. Esperate un ratito.👿')
   }, []);
 
   const fetchIpData = useCallback(async () => {
+    // This not make sense. If the IpAPI has a method named getLocationInfo,
+    // it should return the info and not a Response object.
+    // Also, you are making the dev to call every time the json method, making it redundant.
+    // -> Reed line 34 and below for the variable name comment.
     const callIpData = await IpAPI.getLocationInfo()
     const dataIP = await callIpData.json()
     setIPData(dataIP)
@@ -27,7 +33,14 @@ function App() {
     console.log('amos loco')
     const [lat, lon] = IPData.loc.split(",");
     console.log([lat, lon])
+    // The variable name lead to a missunderstanding because you are not saying what contains the variable.
+    // Would be nices if it was called like callWeatherResponse or just response.
+    // It is a standard that a response variable has a json method.
+    // const response = await WeatherAPI.getWeatherInfo(lat, lon)
+    // const dataWeather = await response.json()
+    // The same applies in the lines 21-22.
     const callWeatherAPI = await WeatherAPI.getWeatherInfo(lat, lon)
+    // Same comment as line 18-20
     const dataWeather = await callWeatherAPI.json()
     setWeatherData(dataWeather)
   }, [IPData])
